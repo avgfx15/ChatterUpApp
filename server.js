a16eabb8af6a6deae0aa5be5f5bf10cd0c23bec6
Ajay Gandhi <avgfx15@gmail.com>
2024-02-26 15:57:27 +0530

    Server Setup for socketIo and View engine setup with ejs

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
 import express from "express"
+import http from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from "./db/db.js";
 
 
 dotenv.config();
 const app = express()
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+app.use(express.static("public"));
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', "POST", "PUT", "DELETE"]
+    }
+})
+
 const port = process.env.PORT
 
 app.get('/', (req, res) => res.send('Hello World!'))
