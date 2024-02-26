104177a889ddf360b0bad7a3adb6f3ab71e72686
Ajay Gandhi <avgfx15@gmail.com>
2024-02-26 19:46:35 +0530

    User Route Structure Create

---
+++ controllers/userControllers.js
@@ -0,0 +1,36 @@
+
+
+export default class UserControllers {
+    // @ GET Login Form
+    getLoginFormController = async (req, res,) => {
+        try {
+
+        } catch (error) {
+            return res.status(500).json({ message: error.message });
+        }
+    }
+    // + UserLogin
+    userLoginController = async (req, res,) => {
+        try {
+
+        } catch (error) {
+            return res.status(500).json({ message: error.message });
+        }
+    }
+    // @ GET Login Form
+    getRegisterationFormController = async (req, res,) => {
+        try {
+
+        } catch (error) {
+            return res.status(500).json({ message: error.message });
+        }
+    }
+    // + User Registeration
+    userRegisterationController = async (req, res,) => {
+        try {
+
+        } catch (error) {
+            return res.status(500).json({ message: error.message });
+        }
+    }
+}
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,35 @@
 import express from 'express';
 import multer from 'multer';
-
+import UserControllers from '../controllers/userControllers.js';
 
 const userRouter = express.Router();
 
-userRouter.get('/')
+const userControllers = new UserControllers();
+
+// @ GET Login Form
+userRouter.get('/', (req, res) => {
+    userControllers.getLoginFormController(req, res)
+})
+
+// + User Login 
+userRouter.post('/', (req, res) => {
+    userControllers.userLoginController(req, res)
+})
+
+// @ GET Registration Form
+userRouter.get('/register', (req, res) => {
+    userControllers.getRegisterationFormController(req, res)
+})
+
+// + User Registration
+userRouter.post('/register', (req, res) => {
+    userControllers.userRegisterationController(req, res)
+})
+
+// @ GET Login Form
+userRouter.get('/', (req, res) => {
+    userControllers.getLoginFormController(req, res)
+})
 
 
 export default userRouter;
